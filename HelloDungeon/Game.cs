5f4fd4cd02aded9f44a84565766ae5a100992f64
drew-aie <159073200+drew-aie@users.noreply.github.com>
2024-08-28 10:37:59 -0500

    Initial project upload

---
+++ HelloDungeon/Game.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloDungeon
+{
+    internal class Game
+    {
+        public void Run()
+        {
+            string playerName = "Boblius";
+            float playerHealth = 10.0f;
+            float playerMana = 5.0f;
+            int playerGold = 3;
+            bool isDead = false;
+
+            Console.WriteLine("Hello, " + playerName);
+            Console.WriteLine();
+            Console.WriteLine("Welcome to my dungeon!");
+            Console.WriteLine();
+            Console.WriteLine("Health: " + playerHealth);
+            Console.WriteLine("Mana: " + playerMana);
+            Console.WriteLine("Gold: " + playerGold);
+            Console.WriteLine();
+            Console.WriteLine(
+                "You find yourself in a generic dungeon " +
+                "with two doors. Which door do you choose?"
+                );
+        }
+    }
+}
