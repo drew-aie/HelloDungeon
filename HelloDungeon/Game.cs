514de6d99f53f513dde855ae9efa50882e27cd5a
drew-aie <159073200+drew-aie@users.noreply.github.com>
2024-08-30 11:31:13 -0500

    Updates

--- HelloDungeon/Game.cs
+++ HelloDungeon/Game.cs
@@ -14,6 +14,7 @@ namespace HelloDungeon
             float playerHealth = 10.0f;
             float playerMana = 5.0f;
             int playerGold = 3;
+            string playerRole = "Warrior";
             bool isDead = false;
 
             Console.WriteLine("Hello, " + playerName);
@@ -24,10 +25,61 @@ namespace HelloDungeon
             Console.WriteLine("Mana: " + playerMana);
             Console.WriteLine("Gold: " + playerGold);
             Console.WriteLine();
-            Console.WriteLine(
-                "You find yourself in a generic dungeon " +
-                "with two doors. Which door do you choose?"
-                );
+            int input = GetInput("Are you a warrior or a wizard?", "Warrior", "Wizard");
+            if (input == 1)
+            {
+                playerRole = "Warrior";
+            }
+            else if (input == 2)
+            {
+                playerRole = "Wizard";
+            }
+
+            Console.WriteLine("Player role: " + playerRole);
+
+
+        }
+
+        int GetInput(string description, string option1, string option2)
+        {
+            string input = "";
+            int inputRecieved = 0;
+
+            // Input loop
+            while (inputRecieved != 1 && inputRecieved != 2)
+            {
+                // Print options
+                Console.WriteLine(description);
+                Console.WriteLine("1. " + option1);
+                Console.WriteLine("2. " + option2);
+                Console.Write("> ");
+
+                // Get input from player
+                input = Console.ReadLine();
+
+                // If player selected the first option
+                if (input == "1" || input == option1)
+                {
+                    // Set inputRecieved to be the first option
+                    inputRecieved = 1;
+                }
+                // Otherwise if the player selected the second option
+                else if (input == "2" || input == option2)
+                {
+                    // Set inputRecieved to be the second option
+                    inputRecieved = 2;
+                }
+                // If neither are true
+                else
+                {
+                    // Display error message
+                    Console.WriteLine("Invalid Input");
+                    Console.ReadKey();
+                }
+            }
+
+            Console.Clear();
+            return inputRecieved;
         }
     }
 }
